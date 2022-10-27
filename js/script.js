4d4111144d0e068da9b991073464958d0c64ae8e
henri-kapidani <h.kapidani@gmail.com>
2022-10-27 15:11:05 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,126 @@
+/*
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+BONUS 1:
+Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
+
+BONUS 2:
+Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
+Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
+
+Prima di partire a scrivere codice:
+Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
+
+Consigli del giorno:
+1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
+2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+
+Buon lavoro e buon divertimento! :slightly_smiling_face:
+*/
+
+const arrImages = [
+	'img/01.jpg',
+	'img/02.jpg',
+	'img/03.jpg',
+	'img/04.jpg',
+	'img/05.jpg',
+];
+
+const eleSliderViewer = document.querySelector('.slider-viewer');
+const eleSliderThumbs = document.querySelector('.thumbs');
+const eleBtnLeft = document.querySelector('.btn-left');
+const eleBtnRight = document.querySelector('.btn-right');
+
+// creare i tag immagine nell'html
+for (let i = 0; i < arrImages.length; i++) {
+	// creare i tag immagine che va nella sezione grande .slider-viewer
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+	eleSliderViewer.append(eleImg);
+
+	// creare i tag immagine che vanno nella sezione .thumbs
+	const eleThumb = document.createElement('img');
+	eleThumb.src = arrImages[i];
+	eleThumb.classList.add('thumb-img');
+	if (i === 0) {
+		eleThumb.classList.add('active');
+	}
+	eleSliderThumbs.append(eleThumb);
+}
+// document.querySelector('.slider-img').classList.add('active');
+
+const listEleImg = document.querySelectorAll('.slider-img'); // non e' un array ma qualcosa di simile
+const listThumbs = document.querySelectorAll('.thumb-img');
+
+let activeIndex = 0;
+document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+document.body.style.backgroundSize = 'cover';
+
+// aggiungere gli event listeners ai due bottoni
+eleBtnRight.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+	// incrementare l'active index con reset per slider infinito
+	/*
+	if (activeIndex === listEleImg.length - 1) {
+		activeIndex = 0;
+	} else {
+		activeIndex++;
+	}
+	*/
+
+	activeIndex++;
+	if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+	document.body.style.backgroundSize = 'cover';
+});
+
+eleBtnLeft.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+	// decrementare l'active index con reset per slider infinito
+	/*
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length - 1;
+	} else {
+		activeIndex--;
+	}
+	*/
+
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
+	activeIndex--;
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+	document.body.style.backgroundSize = 'cover';
+});
